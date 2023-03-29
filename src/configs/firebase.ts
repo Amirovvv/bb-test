a285a0889a7a543bac4690f2b6f28e16157c612b
Khasan Amirov <58747052+Amirovvv@users.noreply.github.com>
2023-03-29 16:09:57 +0300

    feat: connect firebase

---
+++ src/configs/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChZeTRkhyj90LbMkPYMQ9WSyzRTjpHlhI",
+  authDomain: "bb-test-6e1f5.firebaseapp.com",
+  projectId: "bb-test-6e1f5",
+  storageBucket: "bb-test-6e1f5.appspot.com",
+  messagingSenderId: "100631941972",
+  appId: "1:100631941972:web:ae6f40415e3bfc6a79aec1"
+};
+
+const app = initializeApp(firebaseConfig);
