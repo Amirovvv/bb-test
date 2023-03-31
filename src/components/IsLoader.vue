9ac2d6e098ee19565faf5f4a475b66fb918d8004
Khasan Amirov <58747052+Amirovvv@users.noreply.github.com>
2023-03-31 16:05:36 +0300

    feat: component Loader

---
+++ src/components/IsLoader.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="loader-container" v-if="isLoading">
+    <div class="loader"></div>
+  </div>
+  <slot v-else></slot>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  isLoading: {
+    type: Boolean,
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+.loader-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.loader {
+  border: 6px solid #f3f3f3;
+  border-top: 6px solid #3498db;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
